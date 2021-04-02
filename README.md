3962f633f0b93d6c212ef3d27061a337602f2618
work <Work@Navarjuns-MBP.hitronhub.home>
2021-04-02 06:28:21 -0700

    assignment starter code

---
+++ README.md
@@ -0,0 +1,8 @@
+# Week 10 Assignment
+
+### Requirements
+- Create a radial area chart with the "total" column in the dataset.
+- You can refer [this](https://observablehq.com/@d3/radial-area-chart)
+- Add the axes as well.
+
+[Data source](https://www.kaggle.com/abcsds/pokemon)
---
+++ scripts/script.js
@@ -0,0 +1,33 @@
+const margin = {t: 50, r:50, b: 50, l: 50};
+const padding = 15;
+const size = {w: 1000, h: 800};
+const svg = d3.select('svg#pie');
+
+svg.attr('width', size.w)
+    .attr('height', size.h);
+
+const containerG = svg.append('g')
+    .classed('container', true)
+    .attr('transform', `translate(${margin.t}, ${margin.l})`);
+
+size.w = size.w - margin.l - margin.r;
+size.h = size.h - margin.t - margin.b;
+
+const columns = ['hp', 'speed', 'attack', 'defense', 'spAtk', 'spDef'];
+const colorScale = d3.scaleOrdinal()
+    .domain(columns)
+    .range(d3.schemeSet2);
+
+d3.csv('data/Pokemon_subset.csv', function(d) {
+    d.total = +d.total;
+    d.hp = +d.hp;
+    d.attack = +d.attack;
+    d.defense = +d.defense;
+    d.spAtk = +d.spAtk;
+    d.spDef = +d.spDef;
+    d.speed = +d.speed;
+    return d;
+})
+.then(function(data) {
+    data = data.sort((a, b) => a.total < b.total);
+});
